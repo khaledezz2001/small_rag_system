ef65dd778ba33686df7faf506aa598f2f4232691
khaled <khaledezz345@gmail,com>
2025-11-22 17:50:31 +0200

    add dotenv import

--- assets/main.py
+++ assets/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 
 app=FastAPI()
 
-@app.get("/test")
-def hi():
-    return{
-        "message":"hi hi im her"
-    }
+app.include_router(base.base_router)
